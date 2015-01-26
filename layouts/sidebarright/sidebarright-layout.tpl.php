859ff15e889498cbcdd0bea8090f4b7d3dbbbc22
tonyhawk127 <hannesgelsdorf@gmail.com>
2015-01-26 18:16:30 +0100

    Neues Layout hinzugefuegt (sidebarright)

---
+++ layouts/sidebarright/sidebarright-layout.tpl.php
@@ -0,0 +1,6 @@
+<div class="l-page">
+	<?php print render($page['header']); ?>
+	<?php print render($page['sidebar']); ?>
+	<?php print render($page['detail']); ?>
+	<?php print render($page['footer']); ?>
+ </div>
